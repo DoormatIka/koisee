8febd400936b3a331c4e48bd209cdfccea3488e2
DoormatIka <yomotouhou@gmail.com>
2026-02-01 01:18:18 +0800

    bug: router memory issues?

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -9,6 +9,10 @@ async def flet_main(page: ft.Page):
     router = Router(page=page)
     router.add_route(route="/", container=entry_page())
 
+    # works properly.
+    page.views.append(ft.View(route="/", controls=[ft.Text(value="hi")]))
+    # check if self.page.views and page.views are in the same memory.
+
     # print(page.route)
     await page.push_route(page.route)
 
--- src/gui/router/router.py
+++ src/gui/router/router.py
@@ -1,30 +1,35 @@
 
 import flet as ft
 
+"""
+BROKEN (assumptions as to why it isn't working):
+
+# https://docs.flet.dev/controls/page/#flet.Page.push_route
+- in user defined route_change(), the example directly appends to views.
+- push_route() fires on_route_change.
+"""
 class Router:
     page: ft.Page
-    routes: dict[str, ft.Container] = {}
     def __init__(self, page: ft.Page):
         self.page = page
+        self.routes: dict[str, ft.Container] = {}
         self.page.on_route_change = self.on_route_change
+        print("Router init.")
 
     def on_route_change(self, e: ft.RouteChangeEvent):
+        print(f"on route change triggered: {e.route}")
         self.page.views.clear()
         container = self.routes.get(e.route)
-        print(e)
         if container == None:
-            return
+            container = ft.Text("404 - Page Not Found.")
 
         view = ft.View(
             route=e.route,
             controls=[container]
         )
+
         self.page.views.append(view)
 
-        self.page.update()
-
-    def pop(self, e: ft.RouteChangeEvent):
-        pass
 
     def add_route(self, route: str, container: ft.Container):
         self.routes[route] = container
