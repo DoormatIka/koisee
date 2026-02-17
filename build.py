7815f2e1be4682f118b1019485236afd1e1a4598
DoormatIka <yomotouhou@gmail.com>
2026-02-17 22:51:01 +0800

    build/feat: added build file to orchestrate distribution

---
+++ build.py
@@ -0,0 +1,24 @@
+
+from pathlib import Path
+import subprocess
+import platform
+
+os_name = platform.system()
+ext = ".exe" if os_name == "Windows" else ""
+
+subprc_res = subprocess.run(["rustc", "--print", "host-tuple"], capture_output=True, text=True, check=True)
+target = subprc_res.stdout.strip()
+if len(target) <= 0:
+    print("Failed to determine platform target triple.")
+
+print(f"======[ BUILDING ON {target} ]=======")
+
+print(f"======[ Building classifer. ]=======")
+classifier_build_path = Path("classifier/").absolute()
+classifier_name = f"sidecar-{target}{ext}"
+classifier_build_cmd = f"uv run nuitka --standalone --plugin-enable=numpy --plugin-enable=anti-bloat --include-package=imagehash --include-module=main --python-flag=no_docstrings --output-filename={classifier_name} main.py"
+
+_ = subprocess.run(["uv", "sync"], cwd=classifier_build_path)
+_ = subprocess.run(classifier_build_cmd.split(" "), cwd=classifier_build_path)
+
+print(f"======[ Transferring python build to tauri. ]=======")
